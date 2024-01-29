a9231f651e32477842874e0b50adea7782036fa6
MaraMincho <seemaster300@naver.com>
2024-01-29 22:45:32 +0900

    Auto stash before rebase of "origin/Develope"

--- iOS/Plugins/DependencyPlugin/ProjectDescriptionHelpers/Dependency+Target.swift
+++ iOS/Plugins/DependencyPlugin/ProjectDescriptionHelpers/Dependency+Target.swift
@@ -11,13 +11,6 @@ import ProjectDescription
 
 public enum Feature: String {
   case writeBoard
-  case home
-  case splash
-  case profile
-  case login
-  case onboarding
-  case record
-  case signUp
 
   public var targetName: String {
     return rawValue.prefix(1).capitalized + rawValue.dropFirst()
@@ -25,18 +18,6 @@ public enum Feature: String {
 }
 
 public extension TargetDependency {
-  static let designSystem: TargetDependency = .project(target: "DesignSystem", path: .relativeToShared("DesignSystem"))
-  static let commonNetworkingKeyManager: TargetDependency = .project(target: "CommonNetworkingKeyManager", path: .relativeToShared("CommonNetworkingKeyManager"))
-  static let trinet: TargetDependency = .project(target: "Trinet", path: .relativeToCore("Network"))
-  static let coordinator: TargetDependency = .project(target: "Coordinator", path: .relativeToCore("Coordinator"))
-  static let combineCocoa: TargetDependency = .project(target: "CombineCocoa", path: .relativeToShared("CombineCocoa"))
-  static let combineExtension: TargetDependency = .project(target: "CombineExtension", path: .relativeToShared("CombineExtension"))
-  static let log: TargetDependency = .project(target: "Log", path: .relativeToShared("Log"))
-  static let keychain: TargetDependency = .project(target: "Keychain", path: .relativeToCore("Keychain"))
-  static let cacher: TargetDependency = .project(target: "Cacher", path: .relativeToCore("Cacher"))
-  static let userInformationManager: TargetDependency = .project(target: "UserInformationManager", path: .relativeToShared("UserInformationManager"))
-  static let auth: TargetDependency = .project(target: "Auth", path: .relativeToShared("Auth"))
-  static let downSampling: TargetDependency = .project(target: "ImageDownsampling", path: .relativeToShared("ImageDownsampling"))
 
   static func feature(_ feature: Feature) -> TargetDependency {
     return .project(
