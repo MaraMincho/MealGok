04fd9eea21ec4d7cf0eb4e007e3c7a07e75c24c3
MaraMincho <seemaster300@naver.com>
2024-01-30 17:48:59 +0900

    기능추가: RoutingFactory 작성

---
+++ iOS/Projects/Core/RoutingFactory/Sources/Protocols/Building.swift
@@ -0,0 +1,13 @@
+//
+//  Building.swift
+//  RoutingFactory
+//
+//  Created by MaraMincho on 1/30/24.
+//  Copyright © 2024 com.maramincho. All rights reserved.
+//
+
+import UIKit
+
+protocol Building: AnyObject {
+  func build() -> UIViewController
+}
---
+++ iOS/Projects/Core/RoutingFactory/Sources/Protocols/Routing.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+protocol Routing: AnyObject {
+  
+  /// ParentRouter: it is ParentRouter,
+  ///
+  /// when you use This, should declare weak.
+  /// if not you will get retain cycle.
+  var parentRouter: Routing? { get set }
+  
+  var navigationController: UINavigationController? { get }
+  
+  var childRouters: [Routing] { get set }
+  
+  func start()
+}
+
+extension Routing {
+  
+  /// Default PopRouter Method
+  func popRouter() {
+    childRouters.forEach{ router in
+      router.popRouter()
+    }
+    childRouters.removeAll()
+    
+    navigationController?.popViewController(animated: false)
+  }
+}
+
