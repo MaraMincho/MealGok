180450540f0ce65e8ccb98248a8d1c70369a9e99
MaraMincho <seemaster300@naver.com>
2024-01-30 12:49:32 +0900

    버그: 버그수정

--- iOS/Scripts/TuistScript.py
+++ iOS/Scripts/TuistScript.py
@@ -2,13 +2,7 @@ import os
 import sys
 
 def to_lower_camel_case(snake_str):
-    # We capitalize the first letter of each component except the first one
-    # with the 'capitalize' method and join them together.
-    camel_string = to_camel_case(snake_str)
-    return snake_str[0].lower() + camel_string[1:]
-    
-def to_camel_case(snake_str):
-    return "".join(x.capitalize() for x in snake_str.lower().split("_"))
+    return snake_str[0].lower() + snake_str[1:]
     
 def modify_dependency_file(feature_name):
     file_path = 'Plugins/DependencyPlugin/ProjectDescriptionHelpers/Dependency+Target.swift'
