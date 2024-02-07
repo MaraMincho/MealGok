4588b3144099a694e6657704e8b90493d3e0d90d
MaraMincho <103064352+MaraMincho@users.noreply.github.com>
2024-02-07 21:37:27 +0900

    삭제: alamofire 제거

--- iOS/Tuist/Dependencies.swift
+++ iOS/Tuist/Dependencies.swift
@@ -6,7 +6,6 @@ let dependencies = Dependencies(
   swiftPackageManager: SwiftPackageManagerDependencies(
     [
       .package(url: "https://github.com/realm/realm-swift", .exact("10.45.3")),
-      .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.8.1")),
     ],
     baseSettings: Settings.settings(configurations: [
       .debug(name: .debug),
