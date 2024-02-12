087dce2b03f2b9306ae6afcc0c712a2b1746d828
MaraMincho <103064352+MaraMincho@users.noreply.github.com>
2024-02-12 20:12:14 +0900

    기능추가: SharedNotificationName 파일 생성

--- iOS/Projects/Core/SharedNotificationName/Sources/Notification.Name+Shared.swift
+++ iOS/Projects/Core/SharedNotificationName/Sources/Notification.Name+Shared.swift
@@ -1,1 +1,17 @@
-// Content of KeepSources.swift
+
+
+import Foundation
+
+public extension Notification.Name {
+  /// 어플리케이션을 초기 상태로 되돌리고 다시 로드하기 위한 NotificationName입니다.
+  static let goHomeAndReBuild: Self = .init("GoHomeAndReBuild")
+  
+  /// 어플리케이션을 첫 화면으로 보내기 위한 NotificationName입니다.
+  static let goHome: Self = .init("GoHome")
+  
+  /// 어플리케이션을 only portrait Mode로 지원하기 위한 NotificationName입니다.
+  static let portraitScreenMode: Self = .init("PortraitScreenMode")
+  
+  /// 어플리케이션을 모든 화면 전환에 대해 지원하기 위한 NotificationName입니다.
+  static let allScreenMode: Self = .init("AllScreenMode")
+}
