0c45942ed18770317de70728e56e9f83b45dec75
MaraMincho <seemaster300@naver.com>
2024-02-12 18:35:38 +0900

    기능추가: ImageDownScaling 및 fileManager로 저장하는 로직 추가

---
+++ iOS/Projects/Shared/ImageManager/Sources/DownSampleProperty.swift
@@ -0,0 +1,45 @@
+//
+//  DownSampleProperty.swift
+//  ImageManager
+//
+//  Created by MaraMincho on 2/12/24.
+//  Copyright © 2024 com.maramincho. All rights reserved.
+//
+
+import UIKit
+
+public struct DownSampleProperty {
+  let size: CGSize
+  let scale: Int
+  init(size: CGSize, scale: Int = 1) {
+    self.size = size
+    self.scale = scale
+  }
+}
+
+extension Data {
+  func downSample(downSampleProperty property: DownSampleProperty?) -> UIImage? {
+    guard let property else { return nil }
+    
+    let imageSourceOption = [kCGImageSourceShouldCache: false] as CFDictionary
+    let data = self as CFData
+    
+    guard let imageSource = CGImageSourceCreateWithData(data, nil) else {
+      return nil
+    }
+    
+    let maxPixel = Swift.max(property.size.width, property.size.height)
+    let downSampleOptions = [
+      kCGImageSourceCreateThumbnailFromImageAlways: true,
+      kCGImageSourceShouldCacheImmediately: true,
+      kCGImageSourceCreateThumbnailWithTransform: true,
+      kCGImageSourceThumbnailMaxPixelSize: maxPixel
+    ] as CFDictionary
+    
+    guard let cgImage = CGImageSourceCreateThumbnailAtIndex(imageSource, 0, downSampleOptions) else {
+      return nil
+    }
+    return UIImage(cgImage: cgImage)
+  }
+}
+
