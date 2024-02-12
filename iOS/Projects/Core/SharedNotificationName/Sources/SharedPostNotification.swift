0844ebc8789a6943494b8e91a83ddbf40c6d0601
MaraMincho <103064352+MaraMincho@users.noreply.github.com>
2024-02-12 20:22:10 +0900

    문서: 주석 추가

--- iOS/Projects/Core/SharedNotificationName/Sources/SharedPostNotification.swift
+++ iOS/Projects/Core/SharedNotificationName/Sources/SharedPostNotification.swift
@@ -9,10 +9,15 @@
 import Foundation
 
 public enum SharedPostNotification {
+  
+  /// "어플리케이션을 첫 화면으로 보내기 위한 NotificationName"을 
+  /// NotificationCenter.default 로 방출합니다.
   public static func goHome() {
     NotificationCenter.default.post(name: .goHome, object: nil)
   }
 
+  /// "어플리케이션을 초기 상태로 되돌리고 다시 로드하기 위한 NotificationName"을
+  /// NotificationCenter.default로 메세지를 방출합니다.
   public static func goHomeAndReBuild() {
     NotificationCenter.default.post(name: .goHomeAndReBuild, object: nil)
   }
