32f8bd5a40d6f581e2233ab02bb3fa6c3eb54637
MaraMincho <103064352+MaraMincho@users.noreply.github.com>
2024-02-13 18:43:07 +0900

    기능추가: ImageData가 있는 TableViewCell을 누르면, 화면이 Present되는 기능 추가

--- iOS/Projects/Features/Profile/Sources/Presentation/View/ProfileViewController+TableViewDelegate.swift
+++ iOS/Projects/Features/Profile/Sources/Presentation/View/ProfileViewController+TableViewDelegate.swift
@@ -13,4 +13,25 @@ extension ProfileViewController: UITableViewDelegate {
     let cellHeight: CGFloat = 80
     return cellHeight
   }
+
+  func tableView(_: UITableView, didSelectRowAt indexPath: IndexPath) {
+    guard
+      let snapShot = dataSource?.snapshot(),
+      let imageDataURLString = snapShot.itemIdentifiers[indexPath.row].imageDateURL,
+      let imageDataURL = URL(string: imageDataURLString)
+    else {
+      return
+    }
+    do {
+      let imageData = try Data(contentsOf: imageDataURL)
+      let vc = UIViewController()
+      let imageView = UIImageView()
+      imageView.image = UIImage(data: imageData)
+      vc.view = imageView
+      present(vc, animated: true)
+    }catch {
+      fatalError("Cant load DataContent")
+    }
+    
+  }
 }
