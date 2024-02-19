d0521c89aa96576e87ef996e062bc5904b581814
MaraMincho <103064352+MaraMincho@users.noreply.github.com>
2024-02-19 17:57:18 +0900

    기능추가: NotificationContent추가

--- iOS/Projects/Features/MealTimer/Sources/Domain/UseCases/TimerLocalNotificationUseCase.swift
+++ iOS/Projects/Features/MealTimer/Sources/Domain/UseCases/TimerLocalNotificationUseCase.swift
@@ -21,30 +21,44 @@ protocol TimerLocalNotificationUseCaseRepresentable {
 
 final class TimerLocalNotificationUseCase: TimerLocalNotificationUseCaseRepresentable {
   private let timeInterval: Double
+  private let notificationContent: NotificationContent
 
-  init(minutes: Int, seconds: Int = 0) {
+  init(minutes: Int, seconds: Int = 0, notificationContent: NotificationContent = .init()) {
     timeInterval = Double(minutes * 60) + Double(seconds)
+    self.notificationContent = notificationContent
   }
 
   private var currentUserNotificationCenter = UNUserNotificationCenter.current()
 
   func addChallengeCompleteNotification(identifier notificationIdentifier: String) {
     let content = UNMutableNotificationContent()
-    content.title = "Lunch time"
-    content.body = "Food is cooked... let's eat!"
+    content.title = notificationContent.notificationContentTitle
+    content.body = notificationContent.notificationContentBody
 
-    let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+    let trigger = UNTimeIntervalNotificationTrigger(timeInterval: timeInterval, repeats: false)
 
     let request = UNNotificationRequest(identifier: notificationIdentifier, content: content, trigger: trigger)
     UNUserNotificationCenter.current().add(request) { error in
       if let error {
         Logger().error("\(error.localizedDescription)")
       }
-      
     }
   }
 
   func removeChallengeCompleteNotification(identifier notificationIdentifier: String) {
     currentUserNotificationCenter.removePendingNotificationRequests(withIdentifiers: [notificationIdentifier])
+  }
+}
+
+struct NotificationContent {
+  let notificationContentTitle: String
+  let notificationContentBody: String
+  
+  init(
+    notificationContentTitle: String = "목표한 시간동안 식사를 하셨습니다!",
+    notificationContentBody: String = "식사를 기록하고, 공유하는 것은 어떨까요?"
+  ) {
+    self.notificationContentTitle = notificationContentTitle
+    self.notificationContentBody = notificationContentBody
   }
 }
