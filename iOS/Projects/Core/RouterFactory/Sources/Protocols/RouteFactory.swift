a0921538d95fa0293e8a8aa02b006cedda3f571c
MaraMincho <103064352+MaraMincho@users.noreply.github.com>
2024-02-27 10:58:34 +0900

    [Feature] RouterFactory Protocol to 상속관계 고민 (#114)
    * 주석: 주석 수정
    
    * 기능추가: RouterFactory 생성
    
    * 리팩토링:  releaseChildCoordinatorIfTopView operator compactMap to filter 변경

--- iOS/Projects/Core/RouterFactory/Sources/Protocols/RouteFactory.swift
+++ iOS/Projects/Core/RouterFactory/Sources/Protocols/RouteFactory.swift
@@ -13,17 +13,39 @@ import UIKit
 
 public protocol RouterFactoriable: Building & Routing {
   var popSubscription: Cancellable? { get set }
+  /// 만약 topViewController가 build해서 나온 UIViewController일 때 child Rotuer들을 해제 합니다.
   func releaseChildCoordinatorIfTopView(buildViewController: UIViewController)
 }
 
 public extension RouterFactoriable {
   func releaseChildCoordinatorIfTopView(buildViewController viewController: UIViewController) {
     popSubscription = navigationController?.publisher(for: \.topViewController)
-      .compactMap { topViewController in
-        return topViewController === viewController ? () : nil
-      }
+      .filter{$0 === viewController}
       .sink { [weak self] _ in
         self?.childRouters = []
       }
+  }
+}
+
+// MARK: - RouterFactory
+
+public class RouterFactory: RouterFactoriable {
+  public var popSubscription: Cancellable?
+  public var parentRouter: Routing?
+  public var navigationController: UINavigationController?
+  public var childRouters: [Routing] = []
+
+  public func build() -> UIViewController {
+    return UIViewController()
+  }
+
+  public func start(build viewController: UIViewController) {
+    releaseChildCoordinatorIfTopView(buildViewController: viewController)
+    navigationController?.pushViewController(viewController, animated: true)
+  }
+
+  init(parentRouter: Routing?, navigationController: UINavigationController?) {
+    self.parentRouter = parentRouter
+    self.navigationController = navigationController
   }
 }
